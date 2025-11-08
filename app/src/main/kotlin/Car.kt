908cb5ca4dd9c9a13dc135b4df067a001604561f
bytetoast <hvagdn@gmail.com>
2025-11-09 00:58:12 +0900

    feat(car): add move method

--- app/src/main/kotlin/Car.kt
+++ app/src/main/kotlin/Car.kt
@@ -8,4 +8,12 @@ class Car(var name: String, var position: Int) {
     fun getPosition(): Int {
         return position
     }
+    
+    fun move() {
+        val randomVal: Int = (0..9).random()
+        if (randomVal >= 4) {
+            position++
+        }
+        println(name + " : " + "-".repeat(position))
+    }
 }
