3db2cdaf46babe9d26b8d57696ab02a0cae6f898
bytetoast <hvagdn@gmail.com>
2025-11-09 01:03:39 +0900

    feat(racemanager): add processRound method

--- app/src/main/kotlin/RaceManager.kt
+++ app/src/main/kotlin/RaceManager.kt
@@ -10,4 +10,9 @@ class RaceManager() {
     fun addCar(carName: String) {
         cars.add(Car(carName, 0))
     }
+
+    fun processRound() {
+        for (car in cars) car.move()
+        println("\n")
+    }
 }
