6f9866f2888a1c69fb4a3f2238b6f57e0505ff37
bytetoast <hvagdn@gmail.com>
2025-11-09 01:04:18 +0900

    feat(racemanager): add getWinners method

--- app/src/main/kotlin/RaceManager.kt
+++ app/src/main/kotlin/RaceManager.kt
@@ -15,4 +15,19 @@ class RaceManager() {
         for (car in cars) car.move()
         println("\n")
     }
+
+    fun getWinners(): String {
+        var max = -1
+        var winners = ArrayList<String>()
+        for (car in cars) {
+            if (car.getPosition() > max) {
+                max = car.getPosition()
+                winners.clear()
+                winners.add(car.getName())
+            } else if (car.getPosition() == max) {
+                winners.add(car.getName())
+            }
+        }
+        return winners.joinToString(", ")
+    }
 }
