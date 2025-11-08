b4779fe9ae48e2e82eadcfaeb170d5b6de549836
bytetoast <hvagdn@gmail.com>
2025-11-08 23:31:51 +0900

    feat(racemanager): introduce RaceManager class

--- src/RaceManager.kt
+++ src/RaceManager.kt
@@ -4,28 +4,4 @@ class RaceManager() {
     fun getCars(): ArrayList<Car> {
         return cars
     }
-
-    fun addCar(carName: String) {
-        cars.add(Car(carName, 0))
-    }
-
-    fun processRound() {
-        for (car in cars) car.move()
-        println("\n")
-    }
-
-    fun getWinners() {
-        val max = -1
-        val winners = ArrayList<String>()
-        for (car in cars) {
-            if (car.getPosition() > max) {
-                max = car.getPosition()
-                winners.clear()
-                winners.add(car.getName())
-            } else if (car.getPosition() == max) {
-                winners.add(car.getName())
-            }
-        }
-        return java.lang.String.join(", ", winners)
-    }
 }
