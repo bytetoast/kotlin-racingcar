c0ed38b412031ad99f4fd081ccf7e91a3f96d268
bytetoast <hvagdn@gmail.com>
2025-11-09 16:13:25 +0900

    fix(car): add @JvmName to resolve JVM signature clash

--- app/src/main/kotlin/Car.kt
+++ app/src/main/kotlin/Car.kt
@@ -1,14 +1,16 @@
 package org.example.app
 
 class Car(var name: String, var position: Int) {
+    @JvmName("userDefined")
     fun getName(): String {
         return name
     }
 
+    @JvmName("userDefined")
     fun getPosition(): Int {
         return position
     }
-    
+
     fun move() {
         val randomVal: Int = (0..9).random()
         if (randomVal >= 4) {
