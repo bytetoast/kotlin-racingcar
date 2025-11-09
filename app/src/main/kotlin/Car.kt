649e00de6030abfa52234811dbf596c13c819dab
bytetoast <hvagdn@gmail.com>
2025-11-09 21:31:57 +0900

    feat(car): add init block for validation

--- app/src/main/kotlin/Car.kt
+++ app/src/main/kotlin/Car.kt
@@ -1,6 +1,12 @@
 package org.example.app
 
-class Car(var name: String, var position: Int) {
+class Car(val name: String) {
+    var position: Int = 0
+
+    init {
+        require(name.length in 1..5) { "자동차의 이름은 1자 이상 5자 이하여야 합니다." }
+    }
+
     @JvmName("userDefined")
     fun getName(): String {
         return name
