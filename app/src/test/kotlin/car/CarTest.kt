272e7a84a5fac17d267a431764de97b6cdabaf56
bytetoast <hvagdn@gmail.com>
2025-11-14 01:52:52 +0900

    test(car): add unit tests for move

---
+++ app/src/test/kotlin/car/CarTest.kt
@@ -0,0 +1,40 @@
+package car
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlin.test.Test
+import org.assertj.core.api.Assertions.assertThat
+import org.example.app.car.Car
+import org.example.app.car.RandomGenerator
+import org.example.app.IOTest
+
+class CarTest: IOTest() {
+    @Test
+    fun `4 이상이 나오면 전진해야 합니다`() {
+        val mockGenerator = mockk<RandomGenerator>()
+        every {
+            mockGenerator.generateRandomNumber()
+        } returns 5
+        val car = Car("move", mockGenerator)
+        car.move()
+        assertThat(output()).isEqualTo("move : -")
+        verify {
+            mockGenerator.generateRandomNumber()
+        }
+    }
+
+    @Test
+    fun `4 미만이 나오면 멈춰야 합니다`() {
+        val mockGenerator = mockk<RandomGenerator>()
+        every {
+            mockGenerator.generateRandomNumber()
+        } returns 2
+        val car = Car("stay", mockGenerator)
+        car.move()
+        assertThat(output()).isEqualTo("stay :")
+        verify {
+            mockGenerator.generateRandomNumber()
+        }
+    }
+}
