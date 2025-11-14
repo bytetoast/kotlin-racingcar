61756a17fc1da6c0eafb7ff6034523cef4489eeb
bytetoast <hvagdn@gmail.com>
2025-11-14 18:21:16 +0900

    test(car): add unit tests for constructing Car

--- app/src/test/kotlin/car/CarTest.kt
+++ app/src/test/kotlin/car/CarTest.kt
@@ -3,9 +3,11 @@ package car
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import kotlin.test.assertFailsWith
 import kotlin.test.Test
 import org.assertj.core.api.Assertions.assertThat
 import org.example.app.car.Car
+import org.example.app.car.DefaultRandomGenerator
 import org.example.app.car.RandomGenerator
 import org.example.app.IOTest
 
@@ -35,6 +37,20 @@ class CarTest: IOTest() {
         assertThat(output()).isEqualTo("stay :")
         verify {
             mockGenerator.generateRandomNumber()
+        }
+    }
+
+    @Test
+    fun `이름 글자수가 5자를 초과하는 자동차는 생성할 수 없습니다`() {
+        assertFailsWith<IllegalArgumentException> {
+            val car = Car("123456", DefaultRandomGenerator())
+        }
+    }
+
+    @Test
+    fun `이름 글자수가 0인 자동차는 생성할 수 없습니다`() {
+        assertFailsWith<IllegalArgumentException> {
+            val car = Car("", DefaultRandomGenerator())
         }
     }
 }
