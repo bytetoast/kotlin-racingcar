e42614100b22ae423e6e67d30a128a54cbcda288
bytetoast <hvagdn@gmail.com>
2025-11-14 16:32:14 +0900

    test(manager): add unit tests for state transition

--- app/src/test/kotlin/manager/RaceManagerTest.kt
+++ app/src/test/kotlin/manager/RaceManagerTest.kt
@@ -1,9 +1,11 @@
 package manager
 
+import org.example.app.manager.NumRoundsState
 import kotlin.test.assertFailsWith
 import kotlin.test.Test
 import org.example.app.manager.RaceManager
 import org.junit.jupiter.api.assertDoesNotThrow
+import kotlin.test.assertEquals
 
 class RaceManagerTest {
     @Test
@@ -28,5 +30,19 @@ class RaceManagerTest {
         assertDoesNotThrow {
             manager.convertToInt("5")
         }
+    }
+
+    @Test
+    fun `올바른 입력값이면 상태가 SUCCESS로 바뀌어야 합니다`() {
+        val manager = RaceManager()
+        manager.validateNumRounds("20")
+        assertEquals(NumRoundsState.SUCCESS, manager.numRoundsState)
+    }
+
+    @Test
+    fun `잘못된 입력값이면 상태가 ERROR로 바뀌어야 합니다`() {
+        val manager = RaceManager()
+        manager.validateNumRounds("!0")
+        assertEquals(NumRoundsState.ERROR, manager.numRoundsState)
     }
 }
